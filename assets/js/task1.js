26a5067ac89fde7235aa038e00272a8367aa7edc
rexarabe <ab28041993@gmail.com>
2020-12-06 10:58:20 +0200

    I add some row in the js code.

--- assets/js/task1.js
+++ assets/js/task1.js
@@ -17,4 +17,27 @@ console.log(`The value of X now is: ${X}`);
 alert(`The value of X now is: ${X}`);
 
 
-//3rd task
+//3rd task know the surface of rectangle.
+alert(`task3`);
+let width, height, surface;
+height=parseFloat(prompt(`please enter the height of the rectangle`));
+width=parseFloat(prompt('please enter the width of the rectangle'));
+surface=height*width;
+console.log(`The surface of rectangle is: ${surface}`);
+alert(`The surface of rectangle is: ${surface}`);
+
+
+//task 4th
+let dokhod,superdokhod, years, yearsmax;
+alert(`task 4th`);
+dokhod = parseFloat(prompt("Enter the sum of the money that you want to stock in our bank, after a year we will add 5% for a year"));
+yearsmax = parseFloat(prompt("For how many years do you want to stock your money?"));
+for(years=0; years<yearsmax; years++){
+    dokhod *=1.05;
+}
+superdokhod = parseInt(dokhod);
+alert(`The sum of the stock of the money is ${superdokhod} , after ${yearsmax}`);
+console.log(`The sum of the stock of the money is ${superdokhod} , after ${yearsmax}`);
+
+// task 5th
+//const now_year= 2020;
