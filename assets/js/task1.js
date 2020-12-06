0e3948743b47199145f1c271abbd1a32c20db228
rexarabe <ab28041993@gmail.com>
2020-12-06 10:34:36 +0200

    Initial commit

---
+++ assets/js/task1.js
@@ -0,0 +1,20 @@
+// 1st Task: Ask the name name of the user and send him greatings on the screen.
+
+let name_user;
+alert(`task 1`);
+name_user  = prompt("What is your name?");
+console.log(`Hello, ${name_user}`);
+alert(`Hello, ${name_user}`);
+
+
+//2nd task: declare x and multiply by 30 and substract by 100 and put the result on the screen.
+
+alert('task2');
+let X;
+X = parseFloat(prompt(`Please enter the value of X:`));
+X = X*30 -100;
+console.log(`The value of X now is: ${X}`);
+alert(`The value of X now is: ${X}`);
+
+
+//3rd task
